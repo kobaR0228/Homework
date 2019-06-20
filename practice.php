d9c70f0964120c62fe009ac04928f286870196e2
EC2 Default User <ec2-user@ip-172-31-34-234.ap-northeast-1.compute.internal>
2019-06-20 10:18:37 +0000

    homeworkPHP/02

---
+++ practice.php
@@ -0,0 +1,36 @@
+<?php 
+$a=3;
+$b=7;
+echo $a + $b;
+echo "<br>";
+
+
+    $array_months=[
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+        ];
+
+echo $array_months["August"];
+
+ $hello="Hello,";
+ $name="Kobayashi";
+ $world="`s World!";
+ echo "<br>"
+ echo $hello.$name.$world;
+ 
+ 
+
+
+
+
+?>
